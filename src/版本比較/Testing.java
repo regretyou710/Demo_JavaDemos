5ae17869b34b33c0d037ac9cd9c83f0465b6f390
regretyou710 <regretyou710@gmail.com>
2020-09-29 00:32:25 +0800

    202009290032

---
+++ src/版本比較/Testing.java
@@ -0,0 +1,22 @@
+package 版本比較;
+
+import java.util.Scanner;
+
+public class Testing {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("v1:");
+		String v1 = sc.nextLine();
+
+		System.out.print("v2:");
+		String v2 = sc.nextLine();
+
+		if (VersionCompareUtil.compareVersion(v1, v2) == 1) {
+			System.out.println("目前是最新版本");
+		} else if (VersionCompareUtil.compareVersion(v1, v2) == 0) {
+			System.out.println("版本相同");
+		} else if (VersionCompareUtil.compareVersion(v1, v2) == -1) {
+			System.out.println("有最新版本");
+		}
+	}
+}
---
+++ src/版本比較/VersionCompareUtil.java
@@ -0,0 +1,54 @@
+package 版本比較;
+
+import java.util.Scanner;
+
+public class VersionCompareUtil {
+	private VersionCompareUtil() {
+	};
+
+	/*
+	 * @param version1 版本1
+	 * 
+	 * @param version1 版本2
+	 * 
+	 * @return 0:相同
+	 * 
+	 * @return 1:version1大於version2
+	 * 
+	 * @return -1:version1小於version2
+	 */
+	public static int compareVersion(String version1, String version2) {
+		// 情況一:新舊版本相同且長度相同
+		if (version1.equals(version2)) {
+			return 0;
+		}
+
+		String[] v1 = version1.split("\\.");
+		String[] v2 = version2.split("\\.");
+		int v1Len = v1.length;
+		int v2Len = v2.length;
+		int baseLen = 0;// 取短的版本長度(取長的版本長度會超出索引邊界)
+		if (v1Len > v2Len) {
+			baseLen = v2Len;
+		} else {
+			baseLen = v1Len;
+		}
+
+		// 情況二:新舊版本不相同且長度相同
+		for (int i = 0; i < baseLen; i++) {
+			if (v1[i].equals(v2[i])) {
+				// 新舊版本的基礎版本相同就跳過
+				continue;
+			} else {
+				return Integer.parseInt(v1[i]) > Integer.parseInt(v2[i]) ? 1 : -1;
+			}
+		}
+
+		// 情況三:因為情況二新舊版本的基礎版本都相同下觸發子版本判斷
+		if (v1Len != v2Len) {
+			return v1Len > v2Len ? 1 : -1;
+		} else {
+			return 0;
+		}
+	}
+}
